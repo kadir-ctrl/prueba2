aeeb86d87e4e0238801748ef2fcc2b6d71524207
kadir <kadircita24@gmail.com>
2025-06-22 22:27:27 -0600

    add function change state button

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //file js
 console.log("Hola mundo");
+
+const boton = document.getElementById("miBoton");
+
+boton.addEventListener("mouseover", () => {
+    boton.style.backgroundColor = "#f44336"; /* Rojo */
+});
+
+boton.addEventListener("mouseout", () => {
+    boton.style.backgroundColor = "#4CAF50"; /* Verde */
+});
